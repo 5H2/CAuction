fd191890b0e7e8ad59848fb61c2b6004146d1d6e
5H2 <97614691+5H2@users.noreply.github.com>
2023-08-11 22:19:36 -0400

    Update and rename items.html to items.jsx

---
+++ items/items.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import { HyperionPurchase } from "./HyperionPurchase";
+import "./items.css";
+
+export const CauctionXyzByHtml = () => {
+  return (
+    <div className="cauction-xyz-by-html">
+      <div className="div">
+        <div className="overlap">
+          <header className="header">
+            <div className="link-heading-c-s">C&#39;s Auction</div>
+          </header>
+          <div className="link">
+            <div className="text-wrapper">Login</div>
+          </div>
+        </div>
+        <div className="overlap-group">
+          <div className="section">
+            <div className="div-image-grid">
+              <div className="row">
+                <div className="crimson-bundle">
+                  <HyperionPurchase
+                    buttonClassName="hyperion-purchase-instance"
+                    className="crimson-armour"
+                    text="Purchase"
+                  />
+                  <div className="overlap-group-2">
+                    <p className="p">
+                      <span className="span">
+                        Crimson Deal (FULL SET)
+                        <br />
+                      </span>
+                      <span className="text-wrapper-2">
+                        1x Crimson Helmet, 1x Crimson Chestplate, 1x Crimson Leggings 1x Crimson Boots
+                      </span>
+                    </p>
+                    <img className="img" alt="Crimson armour" src="crimson-armour-bundle-image.png" />
+                    <p className="crimson-armour-2">
+                      <span className="text-wrapper-3">$300.00</span>
+                      <span className="text-wrapper-4">&nbsp;</span>
+                      <span className="text-wrapper-5">$275.00</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="overlap-2">
+                  <div className="divans-drill">
+                    <div className="divans-drill-text">Divan’s Drill</div>
+                    <div className="text-wrapper-6">$300.00</div>
+                    <HyperionPurchase className="design-component-instance-node" text="Purchase" />
+                    <img className="img-2" alt="Divans drill image" src="divans-drill-image.png" />
+                  </div>
+                  <div className="space-helmet">
+                    <div className="space-helmet-text">Space Helmet</div>
+                    <div className="text-wrapper-6">$180.00</div>
+                    <HyperionPurchase className="design-component-instance-node" text="Purchase" />
+                    <img className="img-3" alt="Space helmet image" src="space-helmet-image.png" />
+                  </div>
+                  <div className="clover-helmet">
+                    <div className="clover-helmet-text">Clover Helmet</div>
+                    <div className="text-wrapper-6">$336.00</div>
+                    <HyperionPurchase className="design-component-instance-node" text="Purchase" />
+                    <img className="clover-helmet-image" alt="Clover helmet image" src="clover-helmet-image.png" />
+                  </div>
+                </div>
+              </div>
+              <div className="row-2">
+                <div className="giant-fishing-rod">
+                  <div className="overlap-group-3">
+                    <img className="giant-fishing-rod-2" alt="Giant fishing rod" src="giant-fishing-rod-image.png" />
+                    <div className="giant-fishing-rod-3">???</div>
+                    <img className="giant-fishing-rod-4" alt="Giant fishing rod" src="giant-fishing-rod-text.png" />
+                  </div>
+                </div>
+                <div className="overlap-3">
+                  <div className="dead-bush-of-love">
+                    <div className="overlap-4">
+                      <img className="dead-bush-of-love-2" alt="Dead bush of love" src="dead-bush-of-love-image.png" />
+                      <div className="dead-bush-of-love-3" />
+                      <div className="dead-bush-of-love-4">Dead Bush of Love</div>
+                      <div className="dead-bush-of-love-5">$444.44</div>
+                      <HyperionPurchase className="dead-bush-of-love-6" text="Purchase" />
+                    </div>
+                    <div className="dead-bush-of-love-7">RARE ITEM</div>
+                  </div>
+                  <div className="warden-helmet">
+                    <img className="img-4" alt="Warden helmet image" src="warden-helmet-image.png" />
+                    <div className="warden-helmet-text">Warden Helmet</div>
+                    <div className="text-wrapper-7">$80.00</div>
+                    <HyperionPurchase className="hyperion-purchase-2" text="Purchase" />
+                  </div>
+                  <div className="gamebreaker">
+                    <img className="img-3" alt="Gamebreaker image" src="gamebreaker-image.png" />
+                    <div className="gamebreaker-text">Game Breaker</div>
+                    <div className="text-wrapper-7">$230.00</div>
+                    <HyperionPurchase className="hyperion-purchase-2" text="Purchase" />
+                  </div>
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap-5">
+                  <div className="judgement-core">
+                    <img className="img-2" alt="Judgement core image" src="judgement-core-image.png" />
+                    <div className="text-wrapper-8">$30.89</div>
+                    <div className="text-wrapper-9">Judgement Core</div>
+                    <HyperionPurchase className="hyperion-purchase-3" text="Purchase" />
+                  </div>
+                  <div className="deathripper">
+                    <img className="img-3" alt="Deathripper image" src="deathripper-image.png" />
+                    <div className="deathripper-text">Deathripper Dagger</div>
+                    <div className="text-wrapper-8">$333.33</div>
+                    <HyperionPurchase className="hyperion-purchase-3" text="Purchase" />
+                  </div>
+                  <div className="terminator">
+                    <img className="img-4" alt="Terminator image" src="terminator-image.png" />
+                    <HyperionPurchase className="hyperion-purchase-3" text="Purchase" />
+                    <div className="text-wrapper-9">Terminator</div>
+                    <div className="text-wrapper-8">$213.26</div>
+                  </div>
+                  <div className="hyperion">
+                    <img className="hyperion-image" alt="Hyperion image" src="hyperion-image.png" />
+                    <div className="hyperion-text">Hyperion</div>
+                    <div className="text-wrapper-8">$460.78</div>
+                    <HyperionPurchase className="hyperion-purchase-3" text="Purchase" />
+                  </div>
+                </div>
+              </div>
+              <div className="heading-items">Items</div>
+              <div className="div-divider" />
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="element-c-s-auction-all-wrapper">
+              <p className="element-c-s-auction-all">
+                <span className="text-wrapper-10">© 2023 C&#39;s Auction. All rights reserved. | </span>
+                <span className="text-wrapper-11">Join our Discord</span>
+              </p>
+            </div>
+          </footer>
+        </div>
+        <div className="nav">
+          <div className="list">
+            <div className="item-link">
+              <div className="pseudo" />
+              <div className="text-wrapper-12">Coins</div>
+            </div>
+            <div className="item-link-2">
+              <div className="pseudo-2" />
+              <div className="text-wrapper-13">Items</div>
+            </div>
+            <div className="item-link-3">
+              <div className="pseudo-3" />
+              <div className="text-wrapper-12">Collectibles</div>
+            </div>
+            <div className="item-link-4">
+              <div className="pseudo-4" />
+              <div className="text-wrapper-12">Discord</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
